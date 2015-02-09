8fbb8cf9d3f907372273a4413f164b62c674589f
Mathijs Mortimer <mathijs@mortimer.nl>
2015-02-09 20:11:17 +0100

    Initial Commit

--- barffap.py
+++ barffap.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 
+
 class Robot:
     """ This class will hold the logic for our robot.
         It's name BaRfFaP stand for Building a Robot for Fun and Profit
-        The brain of the robot is a raspberry Pi and the low level 
+        The brain of the robot is a raspberry Pi and the low level
         electronic are handled by an Arduino. The Arduino provides an
         interface to the DC motors and various sensors """
 
@@ -12,3 +13,16 @@ class Robot:
 
         print "Hello World!"
         pass
+
+
+def main():
+    """ This is the main function of our script.
+        It will only contain a very limited program
+        logic. The main action happens in the Robot class """
+
+    BaRfFap = Robot()
+    BaRfFap.run()
+
+
+if __name__ == '__main__':
+    main()
