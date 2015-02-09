a47ee5297aed2cab67331e01a48795a6ec56c48f
Mathijs Mortimer <mathijs@mortimer.nl>
2015-02-09 21:18:12 +0100

    Added four motors to Robot class and adjusted run() to make the robot spin constantly

--- barffap.py
+++ barffap.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+# imports
+from arduino_motor_driver import Motor
+
 # Motor ID definitions
 FRONT_LEFT_MOTOR_ID = 1
 FRONT_RIGHT_MOTOR_ID = 2
@@ -14,22 +17,26 @@ class Robot:
         electronic are handled by an Arduino. The Arduino provides an
         interface to the DC motors and various sensors """
 
-    def __init__(self,
-                 frontLeftMotorID, frontRightMotorID,
+    def __init__(self, frontLeftMotorID, frontRightMotorID,
                  rearLeftMotorID, rearRightMotorID):
         """ This function initialises our robot.
             It only requires the various motor IDs for now """
 
-        self.frontLeftMotorID = frontLeftMotorID
-        self.frontRightMotorID = frontRightMotorID
-        self.rearLeftMotorID = rearLeftMotorID
-        self.rearRightMotorID = rearRightMotorID
-
+        self.frontLeftMotor = Motor(frontLeftMotorID)
+        self.frontRightMotor = Motor(frontRightMotorID)
+        self.rearLeftMotor = Motor(rearLeftMotorID)
+        self.rearRightMotor = Motor(rearRightMotorID)
 
     def run(self):
-        """ This function contains the main Robot loop """
+        """ This function contains the main Robot loop. 
+            as an initial test we're going to get the 
+            robot to spin constantly """
 
-        print "Hello World!"
+        print "The (hello) world keeps spinning!!"
+        self.frontLeftMotor.setSpeed(255)
+        self.frontRightMotor.setSpeed(-255)
+        self.rearLeftMotor.setSpeed(255)
+        self.rearRightMotor.setSpeed(-255)
 
 
 def main():
@@ -37,7 +44,7 @@ def main():
         It will only contain a very limited program
         logic. The main action happens in the Robot class """
 
-    BaRfFap = Robot(FRONT_LEFT_MOTOR_ID, 
+    BaRfFap = Robot(FRONT_LEFT_MOTOR_ID,
                     FRONT_RIGHT_MOTOR_ID,
                     REAR_LEFT_MOTOR_ID,
                     REAR_RIGHT_MOTOR_ID)
