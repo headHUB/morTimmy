aedcbec6b1b0096c40f6539c1bd34ed712fa8b35
Mathijs Mortimer <mathijs@mortimer.nl>
2015-02-10 20:33:54 +0100

    Adjusted sendCommand function to accept fixed byte sized command and data fields. These will be sent to the Arduino for processing

--- morTimmy/arduino_serial_hardware_control.py
+++ morTimmy/arduino_serial_hardware_control.py
@@ -3,6 +3,9 @@
 from hardware_control import HardwareController
 import serial
 
+COMMAND_SIZE = 4        # Size of serial command in bytes
+DATA_SIZE = 12          # Size of data in bytes
+
 
 class ArduinoSerialController(HardwareController):
     """ This class is an abstraction layer to allow communication
@@ -24,16 +27,33 @@ class ArduinoSerialController(HardwareController):
             self.channel = None
             print ("Error: Could not establish serial connection "
                    "to %s.\n%s" % (communicationChannel, e))
-            exit()
 
-    def setCommand(self, module, moduleID, command):
-        """ This function will send a command to the specified module """
+    def sendCommand(self, command, data=''):
+        """ This function will send a command to the specified module.
+            The command and data size will be predetermined and combined
+            into a single string. This string will be sent to the arduino
+            for parsing.
 
-        serialCommand = ':'.join(module, moduleID, command)
-        self.channel.write(serialCommand)
+            A command can be less than COMMAND_SIZE and will
+            add trailing whitespaces to meet the required COMMAND_SIZE.
+
+            The data is not mandatory. If no data is provided it will
+            be padded with whitespaces to meet the required DATA_SIZE.
+        """
+
+        if len(command) > COMMAND_SIZE:
+            print ("Error: command %s is invalid. Size should be %d"
+                   "or smaller" % (command, COMMAND_SIZE))
+        if len(data) > DATA_SIZE:
+            print ("Error: data %s is invalid. Size should be %d"
+                   "or smaller" % (data, DATA_SIZE))
+
+        serialCommand = ''.join([command.ljust(COMMAND_SIZE, ' '),
+                                data.ljust(DATA_SIZE, ' ')])
+        # self.channel.write(serialCommand)
         print serialCommand
 
-    def getCommand(self, module):
+    def recvCommand(self, module):
         """ This function will retrieve data from the specified module """
         return
 
@@ -44,7 +64,10 @@ def main():
     """
 
     arduinoControl = ArduinoSerialController()
-    arduinoControl.sendCommand('Motor', '1', '255')
+    arduinoControl.sendCommand('FWD', '255')
+    arduinoControl.sendCommand('STOP')
+    arduinoControl.sendCommand('FAULTY COMMAND')
+
 
 if __name__ == '__main__':
     main()
